d43e9dcfcd15a81c905eaf8aa67224974c4c3897
Alex Zaddach <mrzmanwiki@gmail.com>
2008-10-26 01:39:51 +0000

    git-svn-id: https://python-wikitools.googlecode.com/svn/trunk@19 52ee0a72-91a9-11dd-ab33-2764402e1b4d

--- Wiki.py
+++ Wiki.py
@@ -24,6 +24,7 @@ class Wiki:
 		self.username = ''
 		self.maxlag = '5'
 		self.useragent = "MediaWiki-API-python/0.1"
+		self.limit = '5000' #  FIXME:There needs to be a way to set this based on userrights
 	
 	def login(self, username, password = False, remember = True):
 		"""
@@ -93,7 +94,6 @@ class Page:
 	followRedir - follow redirects (check must be true)
 	"""	
 	def __init__(self, wiki, title, check=True, followRedir = True):
-		self.limit = '5000' #  FIXME:There needs to be a way to set this based on userrights
 		self.wiki = wiki
 		self.title = title
 		self.wikitext = ''
@@ -174,7 +174,7 @@ class Page:
 			'action': 'query',
 			'prop': 'templates',
 			'pageids': self.pageid,
-			'tllimit': self.limit,
+			'tllimit': self.wiki.limit,
 		}
 		req = API.APIRequest(self.wiki, params)
 		response = req.query()
@@ -325,6 +325,75 @@ class Page:
 		response = req.query()
 		token = response['query']['pages'][self.pageid][type+'token']
 		return token
+
+class Category(Page):
+	"""
+	A category on the wiki
+	title should be the full title, including "Category:"
+	"""
+	def __init__(self, wiki, title, check=True, followRedir = False):
+		self.wiki = wiki
+		self.title = title
+		self.wikitext = ''
+		self.templates = ''
+		self.members = []
+		self.pageid = 0 # The API will set a negative pageid for bad titles
+		self.exists = True # If we're not going to check, assume it does
+		if check:
+			self.setPageInfo(followRedir)
+			
+	def getAllMembers(self, titleonly=False, reload=False):
+		"""
+		Gets a list of pages in the category
+		titleonly - set to True to only create a list of strings,
+		else it will be a list of Page objects
+		reload - reload the list even if it was generated before
+		"""
+		if self.members and not reload:
+			return self.members
+		else:
+			self.members = []
+			for page in self.__getMembersInternal(titleonly):
+				self.members.append(page)
+			return self.members
+	
+	def getAllMembersGen(self, titleonly=False, reload=False):
+		"""
+		Generator function for pages in the category
+		titleonly - set to True to return strings,
+		else it will return Page objects
+		reload - reload the list even if it was generated before
+		"""
+		if self.members and not reload:
+			for page in self.members:
+				yield page
+		else:
+			self.members = []
+			for page in self.__getMembersInternal(titleonly):
+				self.members.append(page)
+				yield page
+	
+	def __getMembersInternal(self, titleonly):
+		params = {'action':'query',
+			'list':'categorymembers',
+			'cmtitle':self.title,
+			'cmlimit':self.wiki.limit,
+			'cmprop':'title'
+		}
+		while True:
+			req = API.APIRequest(self.wiki, params)
+			data = req.query()
+			for page in data['query']['categorymembers']:
+				if titleonly:
+					yield page['title']
+				else:
+					yield Page(self.wiki, page['title'], check=False, followRedir=False)
+			try:
+				params['cmcontinue'] = data['query-continue']['categorymembers']['cmcontinue']
+			except:
+				break 
 		
-		
+			
+			
+
 		
