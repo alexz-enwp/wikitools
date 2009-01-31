470fd4a2aa860c88325cf02c5c37f5b98a99672c
Alex Zaddach <mrzmanwiki@gmail.com>
2009-01-31 05:58:53 +0000

    add README and setup.py script included in release
    git-svn-id: https://python-wikitools.googlecode.com/svn/trunk@115 52ee0a72-91a9-11dd-ab33-2764402e1b4d

---
+++ setup.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Wikitools',
+      version='0.1',
+      description='Python module for interacting with a MediaWiki wiki',
+	  long_description = """A Python module for interacting with a MediaWiki wiki using the MediaWiki API.
+		Designed for MediaWiki version 1.14 and higher, should work on 1.13, older versions may have bugs.
+		The edit-API must be enabled on the site to use editing features.
+		This is the first release and may have bugs, especially when dealing with non-ASCII text.""",
+      author='Alex Z. (User:Mr.Z-man @ en.wikipedia)',
+      author_email='mrzmanwiki@gmail.com',
+      url='http://code.google.com/p/python-wikitools/',
+	  license='GPL v3',
+      packages=['wikitools'],
+     )
