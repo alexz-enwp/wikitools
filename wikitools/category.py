a128b47a4efb860fb6f11c91609568cf7a7ff854
Alex Zaddach <mrzmanwiki@gmail.com>
2009-01-31 01:56:21 +0000

    GPL boilerplate stuff
    git-svn-id: https://python-wikitools.googlecode.com/svn/trunk@112 52ee0a72-91a9-11dd-ab33-2764402e1b4d

--- wikitools/category.py
+++ wikitools/category.py
@@ -1,4 +1,20 @@
 ﻿# -*- coding: utf-8 -*-
+# Copyright 2008, 2009 Mr.Z-man
+
+# This file is part of wikitools.
+# wikitools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+ 
+# wikitools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+ 
+# You should have received a copy of the GNU General Public License
+# along with wikitools.  If not, see <http://www.gnu.org/licenses/>.
+
 import wiki, page, api
 
 class Category(page.Page):
