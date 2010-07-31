006a11711231aa43f963f299f579d133392546f0
Alex Zaddach <mrzmanwiki@gmail.com>
2010-07-31 23:35:37 +0000

    add URL to documentation
    git-svn-id: https://python-wikitools.googlecode.com/svn/trunk@367 52ee0a72-91a9-11dd-ab33-2764402e1b4d

--- wikitools/api.py
+++ wikitools/api.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with wikitools.  If not, see <http://www.gnu.org/licenses/>.
 
+# This module is documented at http://code.google.com/p/python-wikitools/wiki/api
+
 import urllib2
 import re
 import time
