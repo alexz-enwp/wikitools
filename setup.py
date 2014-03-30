0eb7585783c480d6252928fbb31ce3357788f1c7
alexz-enwp <mrzmanwiki@gmail.com>
2014-03-30 16:17:14 -0400

    update for 1.2 release

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@
 from distutils.core import setup
 
 setup(name='wikitools',
-      version='1.1.1',
+      version='1.2',
       description='Python package for interacting with a MediaWiki wiki',
 	  long_description = """A Python package for interacting with a MediaWiki wiki using the MediaWiki API.
-Designed for MediaWiki version 1.16 and higher, should work on 1.13, older
-versions may have bugs.
+Designed for MediaWiki version 1.20 and higher, should work on 1.13+.
 The edit-API must be enabled on the site to use editing features.
-Please report any bugs to <http://code.google.com/p/python-wikitools/issues>""",
-      author='Alex Z. (User:Mr.Z-man @ en.wikipedia)',
+Please report any bugs to <https://github.com/alexz-enwp/wikitools/issues>""",
+      author='Alex Zaddach (User:Mr.Z-man @ en.wikipedia)',
       author_email='mrzmanwiki@gmail.com',
-      url='http://code.google.com/p/python-wikitools/',
+      url='https://github.com/alexz-enwp/wikitools',
 	  license='GPL v3',
       packages=['wikitools'],
 	  package_data={'wikitools': ['wikitools/COPYING', 'wikitools/README', 'wikitools/CHANGELOG']},
